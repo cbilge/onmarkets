577fe84c7e9f5ab792083d1d2094feb6c770f4cb
Cem Bilge <Cem Bilge>
2016-02-25 07:21:52 +0200

    fix

--- js/services.js
+++ js/services.js
@@ -4,5 +4,5 @@ onmServices.factory('Feed', ['$resource',
 	function($resource) {
 		return $resource('https://s3.amazonaws.com/codecademy-content/courses/ltp4/photos-api/photos.json'), {}, {
 			query: {method:'GET', params:{}, isArray:true}
-		});	   
+		}	   
 	}]);
