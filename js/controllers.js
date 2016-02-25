fece7b0d822124a5b44b501f530940db9aa8b337
Cem Bilge <cbilge@gmail.com>
2016-02-25 22:50:29 +0200

    "front page almost done"

--- js/controllers.js
+++ js/controllers.js
@@ -1,7 +1,6 @@
 var onmControllers = angular.module('onmControllers', []);
 
 onmControllers.controller('HomeController', ['$scope', 'Daily', '$http', function ($scope, Daily, $http) {
-    $scope.message = "Hello World";
     $scope.daily = [];
     init();
     function init() {
@@ -13,20 +12,45 @@ onmControllers.controller('HomeController', ['$scope', 'Daily', '$http', functio
     $scope.calColor = "#F39C11";
     $scope.emColor = "#C0392B";
     $scope.newsColor = "#3498DB";
+
+    $scope.getColor = function (mod) {
+        if (mod == 1) {
+            return "uptick";
+        }
+        else if (mod == -1) {
+            return "downtick";
+        }
+        else {
+            return "";
+        }
+    }
+
+    $scope.dateFix = function (dt) {
+        console.log(dt);
+
+        var d = Date.parse(dt);
+        return d;
+    }
+
+    $scope.timeFix = function (time) {
+        var d = new Date(time * 86400000);
+        return d;
+    }
+
 } ]);
 
 onmControllers.controller('OnController', function($scope) {
-	$scope.message = "Hello World";
+   $scope.onColor = "#00B0F0";
 });
 
 onmControllers.controller('CalController', function($scope) {
-	$scope.message = "Hello World";
+    $scope.calColor = "#F39C11";
 });
 
 onmControllers.controller('EmController', function($scope) {
-	$scope.message = "Hello World";
+    $scope.emColor = "#C0392B";
 });
 
-onmControllers.controller('NewsController', ['$scope', 'Feed', function($scope, Feed) {
-	$scope.feed = Feed.query();
+onmControllers.controller('NewsController', ['$scope', function($scope) {
+	 $scope.newsColor = "#3498DB";
 }]);
