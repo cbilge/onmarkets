0120c806a2f385d98a398feb43079d8028f713e4
Cem Bilge <cbilge@gmail.com>
2016-02-25 16:33:56 +0200

    asd

--- js/controllers.js
+++ js/controllers.js
@@ -1,9 +1,17 @@
 var onmControllers = angular.module('onmControllers', []);
 
-onmControllers.controller('HomeController', ['$scope', 'Daily', function($scope, Daily) {
-	$scope.message = "Hello World";
-    $scope.synopsis = Daily.query();
-}]);
+onmControllers.controller('HomeController', ['$scope', 'Daily', function ($scope, Daily) {
+    $scope.message = "Hello World";
+    $scope.get = function () {
+        $scope.items = Daily.ajaxItems();
+        //the model returns a promise and THEN items
+        $scope.items.then(function (items) {
+            $scope.items = items;
+        }, function (status) {
+            console.log(status);
+        });
+    };
+} ]);
 
 onmControllers.controller('OnController', function($scope) {
 	$scope.message = "Hello World";
--- js/services.js
+++ js/services.js
@@ -1,8 +1,18 @@
-var onmServices = angular.module('onmServices', ['ngResource']);
+var onmServices = angular.module('onmServices', []);
 
-onmServices.factory('Daily', ['$resource',
-	function($resource) {
-		return $resource('js/data/daily.json', {}, {
-			    query: {method:'GET', params:{}, isArray:true}
-		    });
-	}]);
+onmServices.factory('Daily', ['$http', '$q', function ($http, $q) {
+    return {
+        ajaxItems: function () {
+            var deferred = $q.defer();
+            $http({ method: "GET", url: "data/daily.txt" })
+                .success(function (data, status, headers, config) {
+                    console.log(data);
+                    deferred.resolve(data);
+                }).error(function (data, status, headers, config) {
+                    console.log(data);
+                    deferred.reject(status);
+                });
+            return deferred.promise;
+        }
+    }
+}]);
