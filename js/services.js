7cff2accdc117278388cd63d72f41376f32d7a02
Cem Bilge <cbilge@gmail.com>
2016-02-25 14:23:40 +0200

    fix

--- js/services.js
+++ js/services.js
@@ -2,7 +2,7 @@ var onmServices = angular.module('onmServices', ['ngResource']);
 
 onmServices.factory('Daily', ['$resource',
 	function($resource) {
-		return $resource('js/data/daily.json', {}, {
+		return $resource('js/data/daily.php', {}, {
 			query: {method:'GET', params:{}, isArray:true}
 		});
 	}]);
