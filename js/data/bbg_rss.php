94d78551a26e30d612697a74347f30eb96d16fc1
Cem Bilge <cbilge@gmail.com>
2016-04-07 19:36:21 +0300

    database improvements

--- js/data/bbg_rss.php
+++ js/data/bbg_rss.php
@@ -18,4 +18,23 @@ function Parse ($url) {
 $bbg = "http://www.newslookup.com/rss/business/bloomberg.rss";
 
 echo Parse($bbg);
+
+$servername = "127.5.102.2";
+$username = "adminn9rBZWt";
+$password = "ys9FljhPItJG";
+$dbname = "php";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+$sql = "SELECT title, description, content, lead_img_url, link, pubDate FROM bbg_rss ORDER BY pubDate DESC LIMIT 30";
+$result = $conn->query($sql);
+
+echo json_encode($result);
+
 ?>
---
+++ js/data/invest.php
@@ -0,0 +1,38 @@
+<?php
+function getTable(){
+$html = "http://ec.forexprostools.com/?columns=exc_currency,exc_actual,exc_forecast,exc_previous&countries=32,37,55,72,22,17,93,14,48,10,35,7,53,38,110,11,26,63,4,5&calType=week&timeZone=63&lang=1"
+
+$dom = new DOMDocument;
+$dom->loadHTML($html);
+$table = $dom->getElementByIdName('ecEventsTable');
+$rows = $table->getElementsByTagName('tr');
+
+$data = array();
+foreach ($rows as $row) {
+    $time = $currency = $name = $actual = $forecast = $previous = "";
+    $cells = $row->getElementsByTagName('td');
+    foreach ($cells as $cell) {
+        $class = $cell->getAttribute('class');
+        if (strpos($class, 'time') !== false) {
+            $time = $cell;
+        } elseif (strpos($class, 'flagCur') !== false){
+            $currency = $cell->nodeValue;
+        } elseif (strpos($class, 'event') !== false){
+            $name = $cell->nodeValue;
+        } elseif (strpos($class, 'act') !== false){
+            $actual = $cell->nodeValue;
+        } elseif (strpos($class, 'fore') !== false){
+            $forecast = $cell->nodeValue;
+        } elseif (strpos($class, 'prev') !== false){
+            $previous = $cell->nodeValue;
+        }
+    }
+    $data[] = array('time' => $time, 'currency' => $currency, 'name' => $name, 'actual' => $actual, 'forecast' => $forecast, 'previous' => $previous);
+}
+
+return $data;
+}
+
+echo getTable();
+
+?>
