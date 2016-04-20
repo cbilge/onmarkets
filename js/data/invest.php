ccc7c1b23522246bb412a66825325a600b97e447
Cem Bilge <cbilge@gmail.com>
2016-04-20 17:02:14 +0300

    invest improvs

--- js/data/invest.php
+++ js/data/invest.php
@@ -1,38 +1,35 @@
 <?php
-function getTable(){
-$html = "http://ec.forexprostools.com/?columns=exc_currency,exc_actual,exc_forecast,exc_previous&countries=32,37,55,72,22,17,93,14,48,10,35,7,53,38,110,11,26,63,4,5&calType=week&timeZone=63&lang=1";
 
-$dom = new DOMDocument;
-$dom->loadHTML($html);
-$table = $dom->getElementByIdName('ecEventsTable');
-$rows = $table->getElementsByTagName('tr');
+$url = "http://ec.forexprostools.com/?columns=exc_flags,exc_currency,exc_importance,exc_actual,exc_forecast,exc_previous&category=_employment,_economicActivity,_inflation,_credit,_centralBanks,_confidenceIndex,_balance,_Bonds&importance=1,2,3&features=datepicker,timezone,timeselector,filters&countries=32,37,55,72,22,17,93,14,48,10,35,7,53,38,110,11,26,63,4,5&calType=day&timeZone=63&lang=1";
 
-$data = array();
-foreach ($rows as $row) {
-    $time = $currency = $name = $actual = $forecast = $previous = "";
-    $cells = $row->getElementsByTagName('td');
-    foreach ($cells as $cell) {
-        $class = $cell->getAttribute('class');
-        if (strpos($class, 'time') !== false) {
-            $time = $cell;
-        } elseif (strpos($class, 'flagCur') !== false){
-            $currency = $cell->nodeValue;
-        } elseif (strpos($class, 'event') !== false){
-            $name = $cell->nodeValue;
-        } elseif (strpos($class, 'act') !== false){
-            $actual = $cell->nodeValue;
-        } elseif (strpos($class, 'fore') !== false){
-            $forecast = $cell->nodeValue;
-        } elseif (strpos($class, 'prev') !== false){
-            $previous = $cell->nodeValue;
+
+# Use the Curl extension to query Google and get back a page of results
+$ch = curl_init();
+$timeout = 5;
+curl_setopt($ch, CURLOPT_URL, $url);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, $timeout);
+$html = curl_exec($ch);
+curl_close($ch);
+
+# Create a DOM parser object
+$dom = new DOMDocument();
+
+# Parse the HTML from Google.
+# The @ before the method call suppresses any warnings that
+# loadHTML might throw because of invalid HTML in the page.
+@$dom->loadHTML($html);
+
+# Iterate over all the <a> tags
+foreach($dom->getElementsByTagName('tr') as $row) {
+        # Show the <a href>
+        echo $row->getAttribute('event_timestamp');
+
+        if ($row->hasAttribute('event_timestamp')){
+            foreach ($row->childNodes as $cell) {
+                #echo $cell->attributes;
+            }
         }
-    }
-    $data[] = array('time' => $time, 'currency' => $currency, 'name' => $name, 'actual' => $actual, 'forecast' => $forecast, 'previous' => $previous);
 }
-
-return $data;
-}
-
-echo getTable();
 
 ?>
