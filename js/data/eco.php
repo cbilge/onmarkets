1a5213a7acd54efebe434c55d000c1350af5604a
cbilge <cbilge@gmail.com>
2016-04-25 21:51:36 +0300

    pfft

--- js/data/eco.php
+++ js/data/eco.php
@@ -13,38 +13,38 @@ if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
 
-$sql = "SELECT * FROM eco WHERE 1";
-//echo $sql;
-$result = $conn->query($sql);
-$rows = array();
-while($r = mysqli_fetch_assoc($result)) {
-    $rowTime = strtotime($r['date']);
+
+#date calculations
+
     $today = strtotime(date('Y-m-d'));
     $workDay = date('w');
-    echo $today;
-    echo $workDay;
     #Workday Adjustments
     $toStart = 1;
     $toEnd = 2;
-    if ($workDay = 5) {
+    if ($workDay == 5) {
         #Friday
         $toEnd = 4;
-    } elseif ($workDay = 6) {
+    } elseif ($workDay == 6) {
         #Saturday
         $toEnd = 3;
-    } elseif ($workDay = 0) {
+    } elseif ($workDay == 0) {
         #Sunday
         $toStart = 2;
-    } elseif ($workDay = 1) {
+    } elseif ($workDay == 1) {
         #Monday
         $toStart = 3;
     }
 
-    $dateStart = $today - 60*60*24*$toStart;
-    $dateEnd = $today + 60*60*24*$toEnd;
+    $dateStart = $today - 86400 * $toStart;
+    $dateEnd = $today + 86400 * $toEnd;
 
-    echo $dateStart;
-    echo $dateEnd;
+$sql = "SELECT * FROM eco WHERE 1";
+//echo $sql;
+$result = $conn->query($sql);
+$rows = array();
+
+while($r = mysqli_fetch_assoc($result)) {
+    $rowTime = strtotime($r['date']);
 
     if ($rowTime > $dateStart && $rowTime < $dateEnd) {
         $rows[] = $r;    
