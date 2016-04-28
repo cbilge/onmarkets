998f0dac24a03330f25c50974ce7f9e485dda129
cbilge <cbilge@gmail.com>
2016-04-28 21:38:22 +0300

    refactoring goes on

--- js/services.js
+++ js/services.js
@@ -64,7 +64,7 @@ onmServices.factory('EconomicCalendar', ['$http', 'EconomicData', function ($htt
 } ]);
 
 
-angular.factory('EconomicData', function () {
+onmServices.factory('EconomicData', function () {
     var EconomicData = function (initData) {
 
         this.initialize = function () {
